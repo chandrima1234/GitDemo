39b33317201fc27e3c0f52654988b1e3d4b3065c
chandrima <cchatterjee19@gmail.com>
2022-01-31 14:47:07 +0530

    first commit

---
+++ src/test/java/QaclickAcademy/SeleniumTest.java
@@ -0,0 +1,17 @@
+package QaclickAcademy;
+import org.testng.annotations.Test;
+
+
+public class SeleniumTest {
+
+	public void BrowserAUtomation()
+	{
+		System.out.println("BrowserAUtomation");	
+	}
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");	
+	}
+
+	
+}
